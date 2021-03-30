219eeddd83ba2ab1089f07c66ccb6ab3f8f51cb2
Brennan Thompson <thompson.brennan@gmail.com>
2021-03-29 23:12:51 -0400

    It took forever but i finally got the int to double to sum on the 3rd case

--- src/org/perscholas/Main.java
+++ src/org/perscholas/Main.java
@@ -1,11 +1,13 @@
 package org.perscholas;
 
+import java.util.function.IntToDoubleFunction;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        //  Declare 2 Int's, sum togehter, print result
+        //  First Case:  Declare 2 Int's, sum togehter, print result
         Integer number1 = 23, number2 = 77;
 
         /*  Creating variable for sum of number1 and number2
@@ -19,7 +21,7 @@ public class Main {
 
 
         /*
-        Write a program that declares 2 double variables, assigns a number to each, and adds them together. Assign the sum to a variable. Print out the result
+        Second Case:  Write a program that declares 2 double variables, assigns a number to each, and adds them together. Assign the sum to a variable. Print out the result
          */
         //Declaring two doubles and giving them both values
         Double num1 = 45.750, num2 = 54.250;
@@ -32,7 +34,30 @@ public class Main {
         //end second case
 
 
+        /*
+        Third Case:  Write a program that declares an integer variable and a double variable, assigns numbers to each,
+        and adds them together. Assign the sum to a variable. Print out the result.
+        What variable type must the sum be?
+         */
+
+        //Declaring an integer and double valued variables
+        //Integer random1 = 65;
+        //Double random2 = 35;
+
+        //You cannot add an integar with a double.  Must Convert Int to double
+        //Float.sum(rando) convertRandom1 = new Double(random1);
+
+        /*Integer intNumber1 = 65;
+        Double doubleNumber1 = 35.23;
+        Double convertInt;
+        convertInt = new Double(intNumber1);
+        doubleNumber2 = Math.addExact(intNumber1,doubleNumber1);
+        */
+        Integer num1Int = 56;
+        Double dubNum = 44.0;
 
 
+        System.out.println("The Sum of " + num1Int + " and " + dubNum + " equals " + Double.sum(Double.valueOf(num1Int), dubNum) + "!!!");
+        //end case
         }
 }
