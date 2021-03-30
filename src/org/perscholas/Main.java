91b32efe7aedc3cf050e8c77c2edfa002fc61013
Brennan Thompson <thompson.brennan@gmail.com>
2021-03-29 21:34:16 -0400

    Getting better at committing and pushing

---
+++ src/org/perscholas/Main.java
@@ -0,0 +1,38 @@
+package org.perscholas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //  Declare 2 Int's, sum togehter, print result
+        Integer number1 = 23, number2 = 77;
+
+        /*  Creating variable for sum of number1 and number2
+        adding value at the creation of integer number3
+         */
+        Integer number3 = number1 + number2;
+
+        //printing out test
+        System.out.println("The Sum of " + number1 + " and " + number2 + " equals " + number3 + "!!!");
+        //end first case
+
+
+        /*
+        Write a program that declares 2 double variables, assigns a number to each, and adds them together. Assign the sum to a variable. Print out the result
+         */
+        //Declaring two doubles and giving them both values
+        Double num1 = 45.750, num2 = 54.250;
+
+        //Declaring double variable to hold the sum of number1 and number2
+        Double num3 = num1 + num2;
+
+        //Printing the result
+        System.out.println("The Sum of " + num1 + " and " + num2 + " equals " + num3 + "!!!");
+        //end second case
+
+
+
+
+        }
+}
